208a2d578f9730acbdcff1ed3e8a20cbf04739c0
brandon plummer <bplummer488@gmaill.com>
2019-10-19 09:26:49 -0500

    Updated Header

--- src/App.js
+++ src/App.js
@@ -1,138 +1,249 @@
 import React, { Component } from 'react';
-import Header from "./Header";
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      articles:[],
+      articles: [],
       date: '',
-      subject: 'Marvel Comics',
+      subject: 'Comics',
       key: '',
-      language: 'n'
+      language: 'n',
+      saved: [],
+      finalArticles: []
     };
   }
 
-  componentDidMount(){
-
+  componentDidMount() {
     //Get Date
     var tempDate = new Date();
-    var today = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate();
+    var today =
+      tempDate.getFullYear() +
+      '-' +
+      (tempDate.getMonth() + 1) +
+      '-' +
+      tempDate.getDate();
     //Set Date
     this.setState({
-      date: today 
+      date: today
     });
 
-    let url = 'https://newsapi.org/v2/everything?q='+this.state.subject+'&from='+this.state.date+'&language=e'+this.state.language+'&sortBy=publishedAt&apiKey=6375d648dfa245cf86fc6795b17b6a25';
+    let url =
+      'https://newsapi.org/v2/everything?q=' +
+      this.state.subject +
+      '&from=' +
+      this.state.date +
+      '&language=e' +
+      this.state.language +
+      '&sortBy=publishedAt&apiKey=6375d648dfa245cf86fc6795b17b6a25';
 
     this.setState({
-      key: url 
+      key: url
     });
 
     fetch(url)
-      .then((response) => {
+      .then(response => {
         return response.json();
       })
-      .then((myJson) => {
-        this.setState({ 
+      .then(myJson => {
+        this.setState({
           articles: myJson.articles
         });
-      });
-    }
-    
-    
-    handleSubmit = (event) => {
 
-      event.preventDefault();
-      
-      this.setState({
-        [event.target.name]: event.target.value
-      })
+        const getArticles = this.state.articles;
+        const getLenght = this.state.articles.length;
+        const newArticles = [];
 
-      this.refetch();
-    }
-
-    handleInputChange = (event) => {
-      event.preventDefault();
-
-      //Check To Make Sure VALUE Is Not White Space
-      if(event.target.value.trim().length > 0){
-
-        //Sets Value And Removes White Space Infront Of Value
-        this.setState({
-          [event.target.name]: event.target.value.trim()
-        });
-      }
-    }
-
-    languageChange = () => {
-      this.setState({language: 's'}, this.refetch );
-
-      
-    }
-
-    languageChangeBack = () => {
-      this.setState({
-        language: 'n'}, this.refetch );
-
-
-    }
-
-    refetch = () =>{
-      let url = 'https://newsapi.org/v2/everything?q='+this.state.subject+'&from='+this.state.date+'&language=e'+this.state.language+'&sortBy=publishedAt&apiKey=6375d648dfa245cf86fc6795b17b6a25';
-
-      this.setState({
-        key: url 
-      });
-
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((myJson) => {
-          this.setState({ 
-            articles: myJson.articles
+        for (let i = 0; i < getLenght; i++) {
+          let id = i;
+          let getArr = getArticles[i];
+          let author = getArr.author;
+          let url = getArr.url;
+          let description = getArr.description;
+          let title = getArr.title;
+          let urlToImage = getArr.urlToImage;
+          newArticles.push({
+            id,
+            author,
+            url,
+            description,
+            title,
+            urlToImage
           });
+        }
+
+        this.setState({
+          finalArticles: newArticles
         });
+      });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+
+    this.refetch();
+  };
+
+  handleInputChange = event => {
+    event.preventDefault();
+
+    //Check To Make Sure VALUE Is Not White Space
+    if (event.target.value.trim().length > 0) {
+      //Sets Value And Removes White Space Infront Of Value
+      this.setState({
+        [event.target.name]: event.target.value.trim()
+      });
     }
+  };
+
+  languageChange = () => {
+    this.setState({ language: 's' }, this.refetch);
+  };
+
+  languageChangeBack = () => {
+    this.setState(
+      {
+        language: 'n'
+      },
+      this.refetch
+    );
+  };
+
+  refetch = () => {
+    let url =
+      'https://newsapi.org/v2/everything?q=' +
+      this.state.subject +
+      '&from=' +
+      this.state.date +
+      '&language=e' +
+      this.state.language +
+      '&sortBy=publishedAt&apiKey=6375d648dfa245cf86fc6795b17b6a25';
+
+    this.setState({
+      key: url
+    });
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({
+          articles: myJson.articles
+        });
+
+        const getArticles = this.state.articles;
+        const getLenght = this.state.articles.length;
+        const newArticles = [];
+
+        for (let i = 0; i < getLenght; i++) {
+          let id = i;
+          let getArr = getArticles[i];
+          let author = getArr.author;
+          let url = getArr.url;
+          let description = getArr.description;
+          let title = getArr.title;
+          let urlToImage = getArr.urlToImage;
+          newArticles.push({
+            id,
+            author,
+            url,
+            description,
+            title,
+            urlToImage
+          });
+        }
+
+        this.setState({
+          finalArticles: newArticles
+        });
+      });
+  };
+
+  saveArticle = x => {
+    let saveArr = this.state.saved;
+    let getArr = this.state.finalArticles[x];
+    let id = this.state.saved.length + 100;
+    let author = getArr.author;
+    let url = getArr.url;
+    let description = getArr.description;
+    let title = getArr.title;
+    let urlToImage = getArr.urlToImage;
+    saveArr.push({
+      id,
+      author,
+      url,
+      description,
+      title,
+      urlToImage
+    });
+    this.setState({
+      saved: saveArr
+    });
+  };
+
+  removeArticle = x => {
+    var getSaved = this.state.saved;
+    this.state.saved.forEach(function(element) {
+      if (element.id === x) {
+        let posititon = getSaved.indexOf(element);
+        getSaved.splice(posititon, 1);
+      }
+    });
+    this.setState({
+      saved: getSaved
+    });
+  };
 
   render() {
+    const articlesMap = this.state.finalArticles.map(item => {
+      return (
+        <div key={item.id} className='col-md-4'>
+          <div className='cards-img'>
+            <a href={item.url}>
+              <img className='img-css' src={item.urlToImage} alt='' />
+            </a>
+            <button
+              className='add-button'
+              onClick={() => this.saveArticle(item.id)}
+            >
+              +
+            </button>
+          </div>
+          <div className='cards'>
+            <h3 className='title'>{item.title}</h3>
+            <h4 className='author'>Written by {item.author}</h4>
+
+            <p>{item.description}</p>
+            <a href={item.url}>Read the full article here...</a>
+          </div>
+        </div>
+      );
+    });
     return (
-      <div className="App">
-        <Header />
-          <div className="container">
-            <div className="form-container">
-              <form onSubmit={this.handleSubmit}>
-                  <input type='text' placeholder='Search' name='subject' onChange={this.handleInputChange}/>
-                  <button>></button>
-              </form>
-              <br/>
-              <button className="langButton" onClick={this.languageChange}>Espanol</button>
-              <button className="langButton"  onClick={this.languageChangeBack}>English</button>
-            </div>
-            
-            {this.state.articles.map((item) => {
-              return(
-                <div key={Math.random()} className="cards">
-                    <div className="cards-top">
-                      <h3 className="title">{item.title}</h3>
-                      <h4 className="author">Written by {item.author}</h4>
-                    </div>
-                    <div className="img-container">
-                      <img className="img-css" src={item.urlToImage} alt=""/>
-                    </div>
-                    <div className="cards-bottom">
-                      <p>{item.description}</p>
-                      <a href={item.url}>Read the full article here...</a>
-                    </div>
-                </div>
-              );
-            })}
+      <div className='App'>
+        <Header
+          changeLanguage={this.languageChange}
+          changeLanguageBack={this.languageChangeBack}
+          removeFromSaved={this.removeArticle}
+          savedArticles={this.state.saved}
+          searchBarSubmit={this.handleSubmit}
+          searchBarChange={this.handleInputChange}
+        />
+        <div className='articles-top'>
+          <div className='container'>
+            <div className='row row-margin'>{articlesMap}</div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,39 +1,163 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (
-            <div style={headerBack}>
-                <div style={headerContainer}>
-                <h1 style={headerStyle}>React.Js.News</h1>
-                <p  style={pStyle}>Powered by: <a className="headerA" href="https://newsapi.org/">Newsapi.org</a></p>
-                </div>
-            </div>
-        )
+  state = {
+    clicked: false,
+    savedclicked: false
+  };
+
+  showMenu = () => {
+    if (this.state.clicked === false) {
+      this.setState({
+        clicked: true
+      });
+    } else {
+      this.setState({
+        clicked: false
+      });
     }
+  };
+
+  showSaved = () => {
+    if (this.state.savedclicked === false) {
+      this.setState({
+        savedclicked: true
+      });
+    } else {
+      this.setState({
+        savedclicked: false
+      });
+    }
+  };
+
+  render() {
+    var navclassName = this.state.clicked
+      ? 'nav-dropdown-show'
+      : 'nav-dropdown';
+    var savedclassName = this.state.savedclicked
+      ? 'nav-dropdown-show scroll'
+      : 'nav-dropdown scroll';
+    var numberOfSaved = this.props.savedArticles.length;
+    const articlesMap = this.props.savedArticles.map(item => {
+      return (
+        <div key={item.id} className='saved-contained'>
+          <div className='saved-card'>
+            <button
+              className='add-button'
+              onClick={this.props.removeFromSaved.bind(null, item.id)}
+            >
+              -
+            </button>
+
+            <h3 className='title'>{item.title}</h3>
+
+            <a href={item.url}>Read the full article here...</a>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div style={headerBack}>
+        <div style={headerContainer}>
+          <button className='nav-button' onClick={() => this.showMenu()}>
+            ☰
+          </button>
+          <h3 style={headerStyle}>React.Js.News</h3>
+          <p style={pStyle}>
+            Powered by:{' '}
+            <a className='headerA' href='https://newsapi.org/'>
+              Newsapi.org
+            </a>
+          </p>
+
+          <div className={navclassName}>
+            <div className='form-container'>
+              <form onSubmit={this.props.searchBarSubmit}>
+                <input
+                  className='login-input'
+                  type='text'
+                  placeholder='e-mail'
+                  name='subject'
+                  onChange={this.props.searchBarChange}
+                />
+                <input
+                  className='pass-input'
+                  type='text'
+                  placeholder='Password'
+                  name='subject'
+                  onChange={this.props.searchBarChange}
+                />
+                <button className='search-button'>></button>
+              </form>
+            </div>
+
+            <div className='form-container'>
+              <form onSubmit={this.props.searchBarSubmit}>
+                <input
+                  className='search-input'
+                  type='text'
+                  placeholder='Search'
+                  name='subject'
+                  onChange={this.props.searchBarChange}
+                />
+                <button className='search-button'>></button>
+              </form>
+            </div>
+            <div className='lang-button-container parent'>
+              <button
+                className='lang-button'
+                onClick={this.props.changeLanguage}
+              >
+                Espanol
+              </button>
+              <button
+                className='lang-button'
+                onClick={this.props.changeLanguageBack}
+              >
+                English
+              </button>
+              <button
+                className='lang-button parent'
+                onClick={() => this.showSaved()}
+              >
+                Saved
+              </button>
+              <div className='child'>
+                <p className='nos-style'>{numberOfSaved}</p>
+              </div>
+            </div>
+            <div className={savedclassName}>{articlesMap}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const headerBack = {
-    background: "grey",
-    margin: "0",
-    boxShadow: "1px 2px 8px rgba(0, 0, 0, 0.25)",
-}
+  position: 'fixed',
+  width: '100%',
+  background: 'grey',
+  zIndex: '1',
+  top: '0',
+  margin: '0',
+  boxShadow: '1px 2px 8px rgba(0, 0, 0, 0.25)'
+};
 
 const headerContainer = {
-    maxWidth: "800px",
-    margin: "0 auto",
-    padding: "10px"
-}
+  maxWidth: '1000px',
+  margin: '0 auto',
+  padding: '10px'
+};
 
 const headerStyle = {
-    color: "white",
-    margin: "0"
-}
+  color: 'white',
+  margin: '0'
+};
 
 const pStyle = {
-    color: "white",
-    margin: "0"
-}
+  color: 'white',
+  margin: '0'
+};
 
-
-export default Header
+export default Header;
