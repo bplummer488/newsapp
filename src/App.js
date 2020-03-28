ff68e212269b6d109491f5c1e484e1126f9190a6
brandon plummer <bplummer488@gmaill.com>
2020-03-28 15:56:47 -0600

    CSS and local save update
    This updates the CSS and the local save functionality allowing for better content storage.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './Header';
+import backupImage from './images/backupImage.gif';
+import bgImage from './images/awesome.gif';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
@@ -17,6 +20,29 @@ class App extends Component {
   }
 
   componentDidMount() {
+    let checkLocal;
+
+    if (localStorage.getItem('local') !== null || '') {
+      checkLocal = localStorage.getItem('local');
+      let localArr = checkLocal.split(',');
+      let getSaved = [];
+      localArr.forEach(function(element, i) {
+        if (i % 2) {
+          let item = {
+            id: i + Math.random() * 100,
+            url: localArr[i - 1],
+            title: element
+          };
+          getSaved.push(item);
+        }
+      });
+      this.setState({
+        saved: getSaved
+      });
+    } else {
+      localStorage.removeItem('local');
+    }
+
     //Get Date
     var tempDate = new Date();
     var today =
@@ -169,77 +195,143 @@ class App extends Component {
   saveArticle = x => {
     let saveArr = this.state.saved;
     let getArr = this.state.finalArticles[x];
-    let id = this.state.saved.length + 100;
-    let author = getArr.author;
+    let id = this.state.saved.length + Math.random() * 100;
     let url = getArr.url;
-    let description = getArr.description;
     let title = getArr.title;
-    let urlToImage = getArr.urlToImage;
     saveArr.push({
       id,
-      author,
       url,
-      description,
-      title,
-      urlToImage
+      title
     });
     this.setState({
       saved: saveArr
     });
+
+    let getLocal;
+    let localArr;
+    let checkTitle = title.split(',');
+    let getTitle = checkTitle.join('');
+    let pushArr = [getTitle, url];
+
+    if (localStorage.getItem('local') !== null || '') {
+      getLocal = localStorage.getItem('local');
+      localArr = getLocal.split(',');
+    } else {
+      localStorage.removeItem('local');
+      localArr = [];
+    }
+
+    pushArr.forEach(function(element) {
+      localArr.push(element);
+    });
+
+    localStorage.setItem('local', localArr);
   };
 
   removeArticle = x => {
-    var getSaved = this.state.saved;
+    let getSaved = this.state.saved;
+    let getLocal = localStorage.getItem('local').split(',');
     this.state.saved.forEach(function(element) {
       if (element.id === x) {
         let posititon = getSaved.indexOf(element);
         getSaved.splice(posititon, 1);
+        getLocal.splice(posititon * 2, 2);
       }
     });
     this.setState({
       saved: getSaved
     });
+
+    localStorage.setItem('local', getLocal);
   };
 
   render() {
     const articlesMap = this.state.finalArticles.map(item => {
+      let image = item.urlToImage !== null ? item.urlToImage : backupImage;
+      let className;
+      let first;
+      let second;
+
+      let style = {
+        color: 'white',
+        background: 'black'
+      };
+
+      let firstbutton = {
+        marginBottom: '10px'
+      };
+
+      if (item.id === 0) {
+        className = 'col-md-8';
+        first = <h4 className='title'>{item.title}</h4>;
+        second = (
+          <a href={item.url}>
+            <img className='img-css' src={image} alt='' />
+          </a>
+        );
+      } else if (item.id === 5) {
+        className = 'col-md-6';
+      } else if (item.id === 6) {
+        className = 'col-md-6';
+      } else if (item.id === 11) {
+        className = 'col-md-8';
+      } else if (item.id === 15) {
+        className = 'col-md-6';
+      } else if (item.id === 16) {
+        className = 'col-md-6';
+      } else {
+        className = 'col-md-4';
+        second = <h4 className='title'>{item.title}</h4>;
+        first = (
+          <a href={item.url}>
+            <img className='img-css' src={image} alt='' />
+          </a>
+        );
+      }
       return (
-        <div key={item.id} className='col-md-4'>
-          <div className='cards-img'>
-            <a href={item.url}>
-              <img className='img-css' src={item.urlToImage} alt='' />
-            </a>
+        <div key={item.id} className={className}>
+          <div className='cards-img' style={style}>
+            {first}
             <button
               className='add-button'
+              style={firstbutton}
               onClick={() => this.saveArticle(item.id)}
             >
               +
             </button>
           </div>
           <div className='cards'>
-            <h3 className='title'>{item.title}</h3>
-            <h4 className='author'>Written by {item.author}</h4>
-
-            <p>{item.description}</p>
-            <a href={item.url}>Read the full article here...</a>
+            {second}
+            <p className='description'>{item.description}</p>
+            <a className='card-link' href={item.url}>
+              Read the full article here...
+            </a>
           </div>
         </div>
       );
     });
     return (
-      <div className='App'>
-        <Header
-          changeLanguage={this.languageChange}
-          changeLanguageBack={this.languageChangeBack}
-          removeFromSaved={this.removeArticle}
-          savedArticles={this.state.saved}
-          searchBarSubmit={this.handleSubmit}
-          searchBarChange={this.handleInputChange}
-        />
-        <div className='articles-top'>
-          <div className='container'>
-            <div className='row row-margin'>{articlesMap}</div>
+      <div>
+        <div className='main-scroll'>
+          <Header
+            changeLanguage={this.languageChange}
+            changeLanguageBack={this.languageChangeBack}
+            removeFromSaved={this.removeArticle}
+            savedArticles={this.state.saved}
+            searchBarSubmit={this.handleSubmit}
+            searchBarChange={this.handleInputChange}
+          />
+          <div className='center-bg'>
+            <div className='fixed halftone'>
+              <img className='bg-image' src={bgImage} alt='background' />
+            </div>
           </div>
+          <div className='articles-top'>
+            <div className='container'>
+              <div className='row row-margin'>{articlesMap}</div>
+            </div>
+          </div>
+          <div className='articles-bottom'></div>
         </div>
       </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
 
   render() {
     var navclassName = this.state.clicked
-      ? 'nav-dropdown-show'
+      ? 'nav-dropdown-show main-nav'
       : 'nav-dropdown';
     var savedclassName = this.state.savedclicked
       ? 'nav-dropdown-show scroll'
@@ -51,46 +51,31 @@ class Header extends Component {
 
             <h3 className='title'>{item.title}</h3>
 
-            <a href={item.url}>Read the full article here...</a>
+            <a className='card-link' href={item.url}>
+              Read the full article here...
+            </a>
           </div>
         </div>
       );
     });
     return (
-      <div style={headerBack}>
-        <div style={headerContainer}>
+      <div className='header'>
+        <div className='container'>
           <button className='nav-button' onClick={() => this.showMenu()}>
             ☰
           </button>
-          <h3 style={headerStyle}>React.Js.News</h3>
-          <p style={pStyle}>
+          <h3 className='header-title'>React.Js.News</h3>
+          <p className='p-title'>
             Powered by:{' '}
             <a className='headerA' href='https://newsapi.org/'>
               Newsapi.org
             </a>
           </p>
-
+          <hr />
           <div className={navclassName}>
-            <div className='form-container'>
-              <form onSubmit={this.props.searchBarSubmit}>
-                <input
-                  className='login-input'
-                  type='text'
-                  placeholder='e-mail'
-                  name='subject'
-                  onChange={this.props.searchBarChange}
-                />
-                <input
-                  className='pass-input'
-                  type='text'
-                  placeholder='Password'
-                  name='subject'
-                  onChange={this.props.searchBarChange}
-                />
-                <button className='search-button'>></button>
-              </form>
-            </div>
-
+            <div onClick={() => this.showMenu()} className='exit'></div>
+          </div>
+          <div className={navclassName}>
             <div className='form-container'>
               <form onSubmit={this.props.searchBarSubmit}>
                 <input
@@ -126,6 +111,7 @@ class Header extends Component {
                 <p className='nos-style'>{numberOfSaved}</p>
               </div>
             </div>
+
             <div className={savedclassName}>{articlesMap}</div>
           </div>
         </div>
@@ -133,31 +119,5 @@ class Header extends Component {
     );
   }
 }
-
-const headerBack = {
-  position: 'fixed',
-  width: '100%',
-  background: 'grey',
-  zIndex: '1',
-  top: '0',
-  margin: '0',
-  boxShadow: '1px 2px 8px rgba(0, 0, 0, 0.25)'
-};
-
-const headerContainer = {
-  maxWidth: '1000px',
-  margin: '0 auto',
-  padding: '10px'
-};
-
-const headerStyle = {
-  color: 'white',
-  margin: '0'
-};
-
-const pStyle = {
-  color: 'white',
-  margin: '0'
-};
 
 export default Header;
